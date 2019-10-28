774e96272f391cf82279a3b53e8ae9845de8fb22
morayg <morayg@morayg.local>
2019-10-28 16:37:57 +0300

    add creaing sub TASKS

--- classes/RedmineIssue.py
+++ classes/RedmineIssue.py
@@ -10,3 +10,9 @@ class RedmineIssue(object):
     def create(self, issueName: str, issueDescription: str):
         result = self.redmine.issue.create(project_id='premier-main', subject=issueName, description=issueDescription)
         return result
+
+    def createSubTasks(self, issueName: str, parentId: int, listSubProjects: list):
+        result = []
+        for project in listSubProjects:
+            result.append(self.redmine.issue.create(project_id=project, subject=issueName, parent_issue_id=parentId))
+        return(result)
